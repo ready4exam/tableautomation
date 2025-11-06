12e94daf917529f58dda49fdb0ae5db7f3db7e4e
ready4exam <ready4urexam@gmail.com>
2025-11-06 13:29:09 +0530

    Create _cors.js

---
+++ api/_cors.js
@@ -0,0 +1,18 @@
+// File: /api/_cors.js
+export function corsHeaders(origin) {
+  const allowedOrigins = [
+    "https://ready4exam.github.io",
+    "https://ready4exam-master-automation.vercel.app",
+    "https://tableautomation-5iuc-git-main-ready4exams-projects.vercel.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+  ];
+
+  return {
+    "Access-Control-Allow-Origin": allowedOrigins.includes(origin)
+      ? origin
+      : "https://ready4exam.github.io",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+}
