2b95cf989a7aa01a5d09f6a65afcb2bf07e9dc1e
ready4exam <ready4urexam@gmail.com>
2025-11-09 20:06:54 +0530

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-export const supabase = createClient(
-  'https://gkyvojcmqsgdynmitcuf.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdreXZvamNtcXNnZHlubWl0Y3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NDQ0OTcsImV4cCI6MjA3NjMyMDQ5N30.5dn5HbXxQ5sYNECS9o3VxVeyL6I6Z2Yf-nmPwztx1hE'
-);
